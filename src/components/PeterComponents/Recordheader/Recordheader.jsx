d2dc3e4b39289abed7e33652c983d924d7f1078d
Victor Emeka Okpe <90395120+Victorbjay@users.noreply.github.com>
2025-09-02 04:54:24 +0100

    Revert "Fixes"

--- src/components/PeterComponents/Recordheader/Recordheader.jsx
+++ src/components/PeterComponents/Recordheader/Recordheader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import "./Recordheader.css";
 function Recordheader({ activeLink, setActiveLink }) {
   let recordLinks = [
